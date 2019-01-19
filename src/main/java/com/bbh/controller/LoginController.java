de66fd4e2d2374b383c184010861c997a92f5939
Brown-Harry Boma <brownharryb@gmail.com>
2019-01-19 23:20:42 +0100

    Prevent login page display after authentication

--- src/main/java/com/bbh/controller/LoginController.java
+++ src/main/java/com/bbh/controller/LoginController.java
@@ -1,11 +1,14 @@
 package com.bbh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import com.bbh.repository.UserRepo;
 
@@ -19,6 +22,10 @@ public class LoginController {
 	@GetMapping
 	public ModelAndView login(Model model, String error, String logout) {
 		ModelAndView mv = new ModelAndView();
+		if(userIsAuthenticated()) {
+			mv.setViewName("redirect:/");
+			return mv;
+		}
 		int count = userRepo.findAll().size();
 		mv.addObject("userCount",count);
 		if(error != null) {
@@ -26,6 +33,11 @@ public class LoginController {
 		}
 		mv.setViewName("auth/login.jsp");
 		return mv;
+	}
+	
+	public boolean userIsAuthenticated() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return !(auth.getPrincipal() instanceof String);
 	}
 
 }
